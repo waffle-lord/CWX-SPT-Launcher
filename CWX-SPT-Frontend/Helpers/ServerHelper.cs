2decbace7601593a0f1df90d717c236dcb4ce2b6
CWX <cwxdev@outlook.com>
2024-07-23 16:49:11 +0100

    fix if side is unknown, fix logic for isconnected, add logic to create and delete profiles, store server as field instead of the servers id

--- CWX-SPT-Frontend/Helpers/ServerHelper.cs
+++ CWX-SPT-Frontend/Helpers/ServerHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Net.Http.Json;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 using ComponentAce.Compression.Libs.zlib;
 using CWX_SPT_Backend.CWX;
@@ -12,10 +13,10 @@ public class ServerHelper
     private static readonly object Lock = new object();
     public List<ServerProfileClass> ProfileList = new List<ServerProfileClass>();
     public ServerInfoClass ServerInfo = new ServerInfoClass();
-    public string ConnectedServerId = "";
+    public ServersClass ConnectedServer = null;
     public bool SelectedProfile = false;
     public HttpClient NetClient = new HttpClient();
-    
+
     public static ServerHelper Instance
     {
         get
@@ -31,10 +32,10 @@ public class ServerHelper
     {
         NetClient.BaseAddress = new Uri("http://" + server.Ip);
         var task = await NetClient.GetAsync("/launcher/ping");
-        
+
         return true;
     }
-    
+
     public async Task<bool> GetServerProfiles(ServersClass server)
     {
         var task = await NetClient.GetAsync("/launcher/profiles");
@@ -51,18 +52,45 @@ public class ServerHelper
         return true;
     }
 
+    public async Task<bool> SendProfileRegister(RegisterRequestClass requestClass)
+    {
+        var task = await NetClient.PutAsync("/launcher/profile/register",
+            new ByteArrayContent(SimpleZlib.CompressToBytes(JsonSerializer.Serialize(requestClass),
+                zlibConst.Z_BEST_COMPRESSION)));
+        
+        var result = SimpleZlib.Decompress(await task.Content.ReadAsByteArrayAsync(), null);
+        Console.WriteLine(result); // currently returns "FAILED | OK"
+        await GetServerProfiles(ConnectedServer); // get them again to repopulate TODO: rework server side API
+        return true;
+    }
+
+    public async Task<bool> SendProfileDelete(ServerProfileClass profile)
+    {
+        NetClient.DefaultRequestHeaders.Add("Cookie", $"PHPSESSID={profile.profileId}");
+        NetClient.DefaultRequestHeaders.Add("SessionId", profile.profileId);
+        
+        var task = await NetClient.GetAsync("/launcher/profile/remove");
+        var result = SimpleZlib.Decompress(await task.Content.ReadAsByteArrayAsync(), null);
+        Console.WriteLine(result); // True or False as a string
+        await GetServerProfiles(ConnectedServer);
+        
+        NetClient.DefaultRequestHeaders.Remove("Cookie");
+        NetClient.DefaultRequestHeaders.Remove("SessionId");
+        return true;
+    }
+
     public void LogoutAndDispose()
     {
         // null profiles
         ProfileList = new List<ServerProfileClass>();
         ServerInfo = new ServerInfoClass();
-        ConnectedServerId = "";
+        ConnectedServer = null;
         SelectedProfile = false;
         NetClient = new HttpClient();
     }
 
-    public void Login(string serverId)
+    public void Login(ServersClass server)
     {
-        ConnectedServerId = serverId;
+        ConnectedServer = server;
     }
 }
