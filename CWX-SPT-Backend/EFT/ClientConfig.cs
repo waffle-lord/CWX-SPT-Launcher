e7db9a63222892504c7e8c94a8a54a2ee8d5afce
waffle.lord <waffle.lord@hotmail.com>
2025-01-19 21:32:10 -0500

    update client config

--- CWX-SPT-Backend/EFT/ClientConfig.cs
+++ CWX-SPT-Backend/EFT/ClientConfig.cs
@@ -2,6 +2,21 @@
 
 public class ClientConfig
 {
-    public string BackendUrl { get; set; }
-    public string Version { get; set; }
+    public string BackendUrl;
+    public string MatchingVersion;
+    public string Version;
+
+    public ClientConfig()
+    {
+        BackendUrl = "http://127.0.0.1:6969";
+        Version = "live";
+        MatchingVersion = "live";
+    }
+
+    public ClientConfig(string backendUrl)
+    {
+        BackendUrl = backendUrl;
+        Version = "live";
+        MatchingVersion = "live";
+    }
 }
